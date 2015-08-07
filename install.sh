dd41fe63662ceff16d5a7b0a662ad8789a11765a
Mike Atkins <mike.atkins@lanetix.com>
2015-08-07 13:18:15 -0400

    Install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install caskroom/cask/brew-cask
+brew cask install virtualbox
+brew cask install chefdk
+brew cask install vagrant
+brew install nvm
+npm install -g npm #get a version of npm with private module support
+source ./lanetix.env
+npm install -g lanetix-setup-env
